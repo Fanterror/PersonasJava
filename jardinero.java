5b282c5ba17451d70dabfa0c27b334ba4e768181
Pacheco <fflorez@uniempresarial.edu.co>
2026-08-28 19:57:23 -0500

    Proyecto 0.5

--- jardinero.java
+++ jardinero.java
@@ -1,1 +1,8 @@
-persona
+public class trabajador extends Persona{
+	String herramienta
+	String planta;
+	public void decirEstudiante() {
+		mostrarDatos();
+		System.out.println("Estoy usando una " + "y estoy plantando un/a: " + planta);
+	}
+}
