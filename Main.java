c0fd7a1b083e7081f609b54d44eb6131853fc7a8
Pacheco <fflorez@uniempresarial.edu.co>
2026-08-28 20:07:58 -0500

    Proyecto 0.7

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+	public static void main(String[] args) {
+		Jardinero jardinero = new jardinero();
+		jardinero.nombre="Carlos";
+		jardinero.edad=43;
+		jardinero.planta="Amorphophallus titanum"
+	}
+}
---
+++ profesor.java
@@ -0,0 +1,7 @@
+public class profesor extends trabajador{
+	String materia;
+	public void decirProfesor() {
+		mostrarTrabajo();
+		System.out.println("Mi materia es: " + materia);
+	}
+}
